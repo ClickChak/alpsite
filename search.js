5875be46bb5ce4b8c19e64f19837e7d047c574c3
Ffmanjul <89925777+Ffmanjul@users.noreply.github.com>
2022-09-27 09:57:22 +0545

    Add files via upload

---
+++ search.js
@@ -0,0 +1,43 @@
+function openPage(){
+	var x = document.getElementById("search").value;
+
+	if (x === "bullet train"){
+		window.open("https://123mkv.bond/2022-movies/bullet-train-full-movie-download-free-dual-audio-hd/")
+	}
+	if (x === "thor love and thunder"){
+		window.open("https://123mkv.bond/2022-movies/thor-love-and-thunder-full-movie-download-free-dual-audio-hd/")
+	}
+	if (x === "dc league super pet"){
+		window.open("https://123mkv.bond/2022-movies/dc-league-of-super-pets-full-movie-download-free-dual-audio-hd/")
+	}
+	if (x === "top gun maverick"){
+		window.open("https://123mkv.bond/2022-movies/nope-full-movie-download-free-dual-audio-hd/")
+	}
+	if (x === "nope"){
+		window.open("https://123mkv.bond/2022-movies/nope-full-movie-download-free-dual-audio-hd/")
+	}
+	if (x === "mnecraft"){
+		window.open("https://tlauncher.org/en/")
+	}
+	if (x === "gta vice city"){
+		window.open("http://oceanofgames.com/gta-vice-city-free-download-1793412/")
+	}
+	if (x === "gta 5, gta v"){
+		window.open("https://ipcgames.com/wait-for-game-download/")
+	}
+	if (x === "kgf 2"){
+		window.open("https://drive.google.com/file/d/1oCMMAeM7aE2n9tgF-PwN0CJnHekDTmco/view?usp=sharing")
+	}
+	if (x === "window 7"){
+		window.open("https://getintopc.com/softwares/operating-systems/windows-7-ultimate-32-64-bit-updated-aug-2020-download-4269111/")
+	}
+	if (x === "prem geet 3"){
+		window.open("https://drive.google.com/file/d/1uilNjgNsNU3s_tA9OvlUSq7VpZCYE7c4/view?usp=sharing")
+	}
+	if (x === "premgeet 3"){
+		window.open("https://drive.google.com/file/d/1uilNjgNsNU3s_tA9OvlUSq7VpZCYE7c4/view?usp=sharing")
+	}
+	else{
+		alert("Sorry, check if there is spelling error and all letter should be in lowercase")
+	}
+}
